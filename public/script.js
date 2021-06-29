1df25b7284e980e3ca63a39d32e3361e042f05d0
Leticia Dias <contatoleticiadia@gmail.com>
2021-06-29 20:24:24 -0300

    fix: correction in paginate function position

--- public/script.js
+++ public/script.js
@@ -1,3 +1,47 @@
+function paginate(selectedPage, totalPages){
+    let pages = [],
+        oldPage
+
+    for(let currentPage = 1; currentPage <= totalPages; currentPage++){
+
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+        const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+        if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage){
+            
+            if(oldPage && currentPage - oldPage > 2){
+                pages.push('...')
+            }
+
+            if(oldPage && currentPage - oldPage == 2){
+                pages.push(oldPage + 1)
+            }
+
+            pages.push(currentPage)
+            oldPage = currentPage
+        }
+    }
+
+    return pages
+}
+
+const pagination = document.querySelector('.pagination')
+const page = +pagination.dataset.page
+const total = +pagination.dataset.total
+const pages = paginate(page, total)
+
+let elements = ''
+
+for(let page of pages){
+    elements += `<a href="#">${page}</a>`
+}
+
+pagination.innerHTML = elements
+
+console.log(pages)
+
+
 function addIngredient(){
     const ingredients = document.querySelector('#ingredients')
     const fieldContainer = document.querySelectorAll('.ingredient')
@@ -45,43 +89,3 @@ function deleteRegisteredForm(){
 deleteRegisteredForm()
 
 
-function paginate(selectedPage, totalPages){
-    let pages = [],
-        oldPage
-
-    for(let currentPage = 1; currentPage <= totalPages; currentPage++){
-
-        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
-        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
-        const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
-
-        if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage){
-            
-            if(oldPage && currentPage - oldPage > 2){
-                pages.push('...')
-            }
-
-            if(oldPage && currentPage - oldPage == 2){
-                pages.push(oldPage + 1)
-            }
-
-            pages.push(currentPage)
-            oldPage = currentPage
-        }
-    }
-
-    return pages
-}
-
-const pagination = document.querySelector('.pagination')
-const page = +pagination.dataset.page
-const total = +pagination.dataset.total
-const pages = paginate(page, total)
-
-let elements = ''
-
-for(let page of pages){
-    elements += `<a href="#">${page}</a>`
-}
-
-pagination.innerHTML = elements
